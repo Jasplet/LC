73aa9fb4200af0b842b9f0671b208148bd96f68e
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-11-19 12:40:56 +0000

    Added Plotting fucntion and optional Save flag

--- demo.py
+++ demo.py
@@ -43,33 +43,54 @@ class Data:
 
         # Make rand arrays
 
-        x = a*np.random.randn(20) + b
+        x1 = a*np.random.randn(20) + b
         #For fun, lets swap a and b round
-        y = b*np.random.randn(20) + a
-        print(x)
+        y1 = b*np.random.randn(20) + a
+
         # Make pandas DataFram
-        df = pd.DataFrame(data={'X_Data': x,'Y_Data':y},index = [i for i in range(0,20)])
+        df = pd.DataFrame(data={'X_Data': x1,'Y_Data':y1},index = [i for i in range(0,20)])
         # Write out the Dataframe
         df.to_csv('data.example',sep = ' ',index=False)
         # Altertative "purists" way
         with open('data.example2','w+') as writer:
             writer.write('X_Data Y_Data')
-            for i,n in enumerate(x): # Iterate through values in x
-                writer.write('{} {} \n'.format(x[i],y[i]))
+            for i,n in enumerate(x1): # Iterate through values in x
+                writer.write('{} {} \n'.format(x1[i],y1[i]))
         # Also make a grid file
 
-        x = np.arange(-5, 5, 0.1)
-        y = np.arange(-5, 5, 0.1)
-        [X,Y] = np.meshgrid(x, y, sparse=True)
-        Z = 10*np.sin(X**2 + Y**2) / (X**2 + Y*2)
+        self.x = np.arange(-5, 5, 0.1)
+        self.y = np.arange(-5, 5, 0.1)
+        [X,Y] = np.meshgrid(self.x, self.y, sparse=True)
+        Z = np.sin(X**2) + np.cos(Y**2)
         # write out this file
         np.savetxt('data.grid',Z,delimiter=',')
 
     def read_data(self):
         ''' Function to read in our example data. File names hard-coded in'''
-        conv = {'Y_Data': lambda x: str(x) }
+        # conv = {'Y_Data': lambda x: str(x) }
+        print('Reading Textfile')
+        self.df = pd.read_csv('data.example',delim_whitespace=True)#converters=conv)
+        print('Reading Grid')
+        self.grid = np.loadtxt('data.grid',delimiter=',')
 
-        self.df = pd.read_csv('data.example',delim_whitespace=True,converters=conv)
+    def plot_data(self,save=False):
 
+        fig, (ax1,ax2) = plt.subplots(1,2,figsize = (15,10))
+        ax1.scatter(x = self.df.X_Data, y = self.df.Y_Data,marker='*',color = 'steelblue')
+        ax1.set_xlabel('X data')
+        ax1.set_ylabel('Y data')
+        ax1.set_ylim([np.floor(self.df.Y_Data.min()),np.ceil(self.df.Y_Data.max())])
+        ax1.set_title('Textfile Data')
+        # named colors can be found here https://matplotlib.org/examples/color/named_colors.html
+
+        ax2.set_title('Gridfile data')
+        C = ax2.contourf(self.x,self.y,self.grid)
+        plt.colorbar(C)
+
+        if save is True:
+            plt.savefig('examplefig.eps',format='eps',dpi=400)
+            plt.close('all')
+        else:
+            plt.show()
 if __name__ == '__main__':
     print('Hello World. I am running from the command line')
